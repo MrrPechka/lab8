cf5971bae45c9fa80caa75fa8cc04a780185aaf8
Huk Anna <gukanna606@gmail.com>
2021-05-27 17:14:19 +0300

    ChatServlet + log in/log out

--- src/main/java/bsu/rfact/java/lab_8/servlet/ChatServlet.java
+++ src/main/java/bsu/rfact/java/lab_8/servlet/ChatServlet.java
@@ -1,4 +1,33 @@
 package bsu.rfact.java.lab_8.servlet;
 
+import bsu.rfact.java.lab_8.entity.ChatMessage;
+import bsu.rfact.java.lab_8.entity.ChatUser;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ChatServlet {
+    private static final long serialVersionUID = 1L;
+    protected HashMap<String, ChatUser> activeUsers;
+    protected ArrayList<ChatMessage> messages;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        activeUsers = (HashMap<String, ChatUser>) getServletContext().getAttribute("activeUsers");
+        messages = (ArrayList<ChatMessage>) getServletContext().getAttribute("messages");
+
+        if (activeUsers == null) {
+            activeUsers = new HashMap<>();
+            getServletContext().setAttribute("activeUsers", activeUsers);
+        }
+        if (messages == null) {
+            messages = new ArrayList<>(100);
+            getServletContext().setAttribute("messages", messages);
+        }
+
+    }
 }
